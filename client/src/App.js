15a68df93e43df57cbff669f4d09fbf1a53a83bf
Srihari Krishnaswamy <srihari@uw.edu>
2023-04-06 20:35:31 -0700

    redux added

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import {Routes, Route} from 'react-router-dom'
+import LayoutScreen from './components/LayoutScreen'
+import Public from './components/Public'
+import Login from './features/auth/Login';
+import DashLayout from './components/DashLayout'
+import Welcome from './features/auth/Welcome'
+import NotesList from './features/notes/NotesList'
+import UsersList from './features/users/UsersList'
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LayoutScreen />} >
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />}/>
+          <Route path = "notes">
+            <Route index element={<NotesList />} />
+          </Route>
+          <Route path = "users">
+            <Route index element={<UsersList />} />
+          </Route>
+        </Route> 
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
+ 
---
+++ client/src/components/LayoutScreen.js
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom'
+
+import React from 'react'
+
+const LayoutScreen = () => {
+  return (
+    <Outlet />
+  )
+}
+
+export default LayoutScreen
